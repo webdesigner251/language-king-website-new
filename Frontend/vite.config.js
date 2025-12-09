fd3ae58683577cc215f1a86818b813eba69d4a35
Kanu3690 <kanu3690@gmail.com>
2025-12-09 15:10:22 +0530

    vite change

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,4 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   base: "/language-king-website-new/",
   plugins: [react(),tailwindcss()],
+  build: {
+    assetsDir: ""
+  }
 })
